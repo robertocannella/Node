100cd5a65fb8b362db2ca48bc2905b808313ebb5
robertocannella <robertocannella@gmail.com>
2022-05-21 16:17:34 -0400

    handing express requests

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 import { createServer } from 'http';
 
 import express from 'express';
-
-import { requestHanderler as routes } from './routes.js';
-import exp from 'constants';
+import bodyParser from 'body-parser';
 
 const app = express();
-app.use((req, res, next) => {
-    console.log("middleware");
-    next();
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+app.use('/add-product', (req, res, next) => {
+    console.log("add product middleware");
+    // -- send response:
+    res.send('<h1>The Add Product Page.</h1><form action="product" method="POST"><input type="text" name="title"><button type="submit">submit</button></form>')
 })
-app.use((req, res, next) => {
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body)
+    res.redirect('/');
+    console.log("product middleware");
+})
+
+app.use('/', (req, res, next) => {
     console.log("another middleware");
+    // -- send response:
+    res.send('<h1>Hello from Express!</h1>')
 })
 
+app.listen(3001, () => {
+    console.log("Listening on port 3001...")
+})
+// const server = createServer(app);
 
-const server = createServer(app);
-
-server.listen(3001);
+// server.listen(3001);
