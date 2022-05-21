a08fa7d37667c780ece317405cd83058551ad014
robertocannella <robertocannella@gmail.com>
2022-05-21 12:14:37 -0400

    text/html response

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ const server = createServer((req, res) => { // Anonymous RequestListener
     console.log(req.headers)
 
 
+    res.setHeader('content-type', 'text/html');
+    res.write('<html>')
+    res.write('<head><title>Response HTML</title></head>')
+    res.write('<body><h1>Repsonse HTML</body>')
+    res.write('</html>')
+    res.end();
     //
     // Exit the Event Loop
     //
