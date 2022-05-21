8c25df8832e4aa05db80e5ded8e7d2a52f306387
robertocannella <robertocannella@gmail.com>
2022-05-21 15:29:12 -0400

    import and contructed express

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 import { createServer } from 'http';
-import { requestHanderler as routes } from './routes.js';
 
-const server = createServer(routes);
+import express from 'express';
+
+import { requestHanderler as routes } from './routes.js';
+import exp from 'constants';
+
+const app = express();
+app.use((req, res, next) => {
+    console.log("middleware");
+    next();
+})
+app.use((req, res, next) => {
+    console.log("another middleware");
+})
+
+
+const server = createServer(app);
 
 server.listen(3001);
