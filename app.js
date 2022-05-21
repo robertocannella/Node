05e4b48f8ff6b6489fe95aefe037fef6c4bac124
robertocannella <robertocannella@gmail.com>
2022-05-21 12:35:07 -0400

    routing requests

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ import { createServer } from 'http';
 
 const server = createServer((req, res) => { // Anonymous RequestListener
     // Event loop
+    const url = req.url;
+    if (url === '/') {
+        res.setHeader('content-type', 'text/html');
+        res.write('<html>')
+        res.write('<head><title>Response HTML</title></head>')
+        res.write('<body><form action="/message" method="POST"><input name="message" type="text"/><button type="submit">Send</button></body>')
+        res.write('</html>')
+        return res.end();
+    }
 
-    console.log(req.url, req.method)
-    console.log(req.headers)
 
 
     res.setHeader('content-type', 'text/html');
